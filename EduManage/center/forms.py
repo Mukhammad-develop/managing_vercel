0f4fe830686b30d2fa00a289e179f74d596bcfb3
Mukhammad-develop <115652500+Mukhammad-develop@users.noreply.github.com>
2023-11-14 20:33:24 +0500

    project

---
+++ EduManage/center/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from django.contrib.auth.models import User
+from . import models
+
+
+class StudentUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'password']
+
+
+class StudentExtraForm(forms.ModelForm):
+    class Meta:
+        model = models.StudentExtra
+        fields = ['cl', 'mobile', 'status']
+
+
+class AdminSigupForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'password']
+
+
+class TeacherUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'password']
+
+
+class TeacherExtraForm(forms.ModelForm):
+    class Meta:
+        model = models.TeacherExtra
+        fields = ['mobile', 'status']
+
+
+class NoticeForm(forms.ModelForm):
+    class Meta:
+        model = models.Notice
+        fields = '__all__'
